6d76ad94ad46e228fd711aa5b98e2f569c9dc25b
SergeySandler <sergey.sandler@catapultsports.com>
2025-04-24 10:14:26 +1000

    0.0.0.63: ice_hockey_mp event is supported

--- R/eventsIMA.R
+++ R/eventsIMA.R
@@ -11,7 +11,7 @@
 #' all_events <- ima_events()
 ima_events <- function() {
     return(c("ima_acceleration", "ima_jump", "ima_jump_ml", "ima_impact", "goalkeeping_v1", "goalkeeping_v2", "cricket_delivery_au", 
-        "cricket_delivery", "running_symmetry", "ice_hockey_stride", "ice_hockey_bout", 
+        "cricket_delivery", "running_symmetry", "ice_hockey_stride", "ice_hockey_bout", "ice_hockey_mp", 
         "baseball_pitch_v1", "baseball_swing_v1", "baseball_pitch", "baseball_swing", "baseball_throw",
         "free_running", "football_movement_analysis",   # former "movement_cluster"
         "rugby_union_scrum", "rugby_union_contact_involvement", "rugby_union_kick", "rugby_union_lineout", "rugby_league_tackle", 
